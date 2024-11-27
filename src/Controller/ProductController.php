5a38a2debe60da03a4a7a686bc9ef2d5777a6d81
jprivet-dev <jean.philippe.rivet@gmail.com>
2024-11-27 15:19:46 +0100

    feat(ProductController): create base of controller

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'product_create')]
+    public function create(ProductRepository $productRepository): Response
+    {
+        $product = new Product();
+        $product->setName('Keyboard');
+        $product->setPrice(1999);
+        $product->setDescription('Ergonomic and stylish!');
+
+        $productRepository->save($product);
+
+        return new Response('Saved new product with id '.$product->getId());
+    }
+
+    #[Route('/product/{id}', name: 'product_show')]
+    public function show(Product $product): Response
+    {
+        return new Response('Check out this great product: '.$product->getName());
+    }
+}
