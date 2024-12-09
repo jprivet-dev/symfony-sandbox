ed3b08995a770ed6440c83d868a9c4f07a6230e1
jprivet-dev <jean.philippe.rivet@gmail.com>
2024-12-09 10:27:40 +0100

    build(composer): install symfony/profiler-pack

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
