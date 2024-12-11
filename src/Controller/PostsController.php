241e0869ac4a12b1a1394ab72a48207f25a2bb56
jprivet-dev <jean.philippe.rivet@gmail.com>
2024-12-11 13:33:22 +0100

    refactor(phpcsfixer): fix

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Post;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
