9a9923786e22afdc3ce2e85d355ec7f9719190f9
jprivet-dev <jean.philippe.rivet@gmail.com>
2024-12-13 19:58:57 +0100

    conf(tailwind): update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,72 @@
+/**
+ * See https://tailwindcss.com/docs/configuration
+ * See https://github.com/timlrx/tailwind-nextjs-starter-blog/blob/main/tailwind.config.js
+ */
+
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      lineHeight: {
+        11: '2.75rem',
+        12: '3rem',
+        13: '3.25rem',
+        14: '3.5rem',
+      },
+      colors: {
+        primary: colors.emerald,
+        gray: colors.gray,
+      },
+      zIndex: {
+        60: '60',
+        70: '70',
+        80: '80',
+      },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: `${theme('colors.primary.600')}`,
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            'h1,h2': {
+              fontWeight: '700',
+              letterSpacing: theme('letterSpacing.tight'),
+            },
+            h3: {
+              fontWeight: '600',
+            },
+            code: {
+              color: theme('colors.indigo.500'),
+            },
+          },
+        },
+        invert: {
+          css: {
+            a: {
+              color: theme('colors.primary.500'),
+              '&:hover': {
+                color: `${theme('colors.primary.400')}`,
+              },
+              code: { color: theme('colors.primary.400') },
+            },
+            'h1,h2,h3,h4,h5,h6': {
+              color: theme('colors.gray.100'),
+            },
+          },
+        },
+      }),
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
