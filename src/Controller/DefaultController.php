f0fa167b0c037fee72c860c8c7688cf1e8e387c9
jprivet-dev <jean.philippe.rivet@gmail.com>
2024-12-17 23:31:49 +0100

    refactor(DefaultController.php): use DefaultController name instead of HomeController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,9 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'app_homepage')]
+    #[Route('/', name: 'app_default')]
     public function index(PostRepository $postRepository): Response
     {
         return $this->render('home.html.twig', [
