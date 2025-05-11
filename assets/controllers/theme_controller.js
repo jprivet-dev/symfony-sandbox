de8fc04b5cda1d84255f67852ab907eda33eb717
jprivet-dev <jprivet.dev@gmail.com>
2025-05-11 23:48:18 +0200

    feat(theme_controller.js): use events

--- assets/controllers/theme_controller.js
+++ assets/controllers/theme_controller.js
@@ -2,20 +2,23 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
     connect() {
-        super.connect();
-        this.activeThemeIcon();
+        document.documentElement.addEventListener('theme:active', (event) => this.activeThemeIcon(event));
+        this.dispatchEvent('init');
     }
 
     next = () => {
-        nextTheme();
-        this.activeThemeIcon();
-    }
+        this.dispatchEvent('next');
+    };
 
-    activeThemeIcon = () => {
-        this.element.querySelector('svg use').setAttribute('href', `#theme-icon-${getStoredTheme()}`);
+    activeThemeIcon = (event) => {
+        this.element.querySelector('svg use').setAttribute('href', `#theme-icon-${event.detail.theme}`);
 
-        const title = `Toggle theme (${getStoredTheme()})`;
+        const title = `Toggle theme (${event.detail.theme})`;
         this.element.setAttribute('aria-label', title);
         this.element.setAttribute('title', title);
-    }
+    };
+
+    dispatchEvent = (name) => {
+        this.dispatch(name, { prefix: 'theme' });
+    };
 }
